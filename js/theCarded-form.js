a983e9eac2af8a40b995bc5d16ca6c407986f0d9
Alan Harnum <waharnum@gmail.com>
2017-07-19 18:01:38 -0400

    Initial commit.

---
+++ js/theCarded-form.js
@@ -0,0 +1,26 @@
+var getYesNoChoice = function () {
+    var choice = {
+        options: ["Yes", "No"],
+        choice: ko.observable("")
+    };
+    return choice;
+};
+
+var viewModel = {
+    whatHappened:
+    {
+        options:
+            [
+                "I was arrested or detained by the police.",
+                "I was asked questions by the police."
+            ],
+        choice: ko.observable("")
+    },
+    questioningCircumstances: getYesNoChoice(),
+    identifyingInformation: getYesNoChoice(),
+    tellYouWhy: getYesNoChoice(),
+    invalidReason: getYesNoChoice(),
+    tellYouRefuse: getYesNoChoice(),
+    offerReceipt: getYesNoChoice()
+};
+ko.applyBindings(viewModel);
